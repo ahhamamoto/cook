e15c69bbca5d447f99ae88a7fbdabd83131c0f01
anderson <anderson.hamamoto@gmail.com>
2013-11-12 18:01:26 -0200

    Classes de Cook e Warehouse adicionados

---
+++ src/Cook/Cook.java
@@ -0,0 +1,14 @@
+package Cook;
+
+/*
+    Lista de coisas a se fazer
+    TODO: adicionar atributos definidos
+    TODO: adicionar os métodos definidos
+    TODO: fazer a interface de Cook (implementando o RMI)
+ */
+
+public class Cook {
+    public static void main(String args[]) {
+        System.out.println("Cook class");
+    }
+}
---
+++ src/Warehouse/Warehouse.java
@@ -0,0 +1,14 @@
+package Warehouse;
+
+/*
+    Lista de coisas a se fazer
+    TODO: adicionar atributos definidos
+    TODO: adicionar os métodos definidos
+    TODO: fazer a interface de Warehouse (implementando o RMI)
+ */
+
+public class Warehouse {
+    public static void main(String args[]) {
+        System.out.println("Warehouse class");
+    }
+}
